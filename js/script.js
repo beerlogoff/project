fbb14928f7aec3ad9276c1477445cf78a1c814aa
Vladimir <vladimir@gmail.com>
2023-09-21 20:18:19 +0300

    My first project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-"use strict";
+"use strict"
 
-const category = 'toys';
-console.log(`http://rozetka/${category}/5`);
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели', '');
 
-const user = 'Ivan'
-alert(`Привет, ${user}`)
+const personalMovieDB = {
+    counr: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Один из последних просмотренных фильмов?', ''),
+    b = prompt ('На сколько оцените его?', ''),
+    c = prompt ('Один из последних просмотренных фильмов?', ''),
+    d  =prompt ('На сколько оцените его?', '');
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
